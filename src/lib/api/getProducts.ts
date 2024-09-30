0d95c73cd3c908b55c0d786d28e277d5958050f7
Mohammad Erfun Ghodoosi <erfunghodoosi@gmail.com>
2024-09-30 19:02:09 +0330

    using try catch

--- src/lib/api/getProducts.ts
+++ src/lib/api/getProducts.ts
@@ -1,13 +1,18 @@
 import { Product } from "@/types/product";
 
 export async function getProducts(): Promise<Product[]> {
-  const res = await fetch(
-    `${
-      process.env.NODE_ENV === "production" ? "" : "http://localhost:3000"
-    }/api/products`
-  );
-  if (!res.ok) {
-    throw new Error("Failed to fetch products");
+  try {
+    const res = await fetch(
+      `${
+        process.env.NODE_ENV === "production" ? "" : "http://localhost:3000"
+      }/api/products`
+    );
+    if (!res.ok) {
+      throw new Error("Failed to fetch products");
+    }
+    return res.json() || [];
+  } catch (error) {
+    console.log(error);
+    return [];
   }
-  return res.json() || [];
 }
