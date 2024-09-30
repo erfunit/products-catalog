a755bc133b548d1241865d61fcb504d409acc54e
Mohammad Erfun Ghodoosi <erfunghodoosi@gmail.com>
2024-09-30 18:57:34 +0330

    handling url

--- src/lib/api/getProducts.ts
+++ src/lib/api/getProducts.ts
@@ -1,9 +1,13 @@
 import { Product } from "@/types/product";
 
 export async function getProducts(): Promise<Product[]> {
-  const res = await fetch("http://localhost:3000/api/products");
+  const res = await fetch(
+    `${
+      process.env.NODE_ENV === "production" ? "" : "http://localhost:3000"
+    }/api/products`
+  );
   if (!res.ok) {
     throw new Error("Failed to fetch products");
   }
-  return res.json();
+  return res.json() || [];
 }
